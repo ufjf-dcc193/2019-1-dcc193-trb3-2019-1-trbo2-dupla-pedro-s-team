9b048ebc6d23a1e5207e91a29426c8807f5255ac
Pedro <pffreitas@stefanini.com>
2019-07-05 10:36:07 -0300

    Commit inicial

---
+++ trabalhoOpcional/src/test/java/com/br/ufjf/dcc193/trabalhoOpcional/trabalhoOpcional/TrabalhoOpcionalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.br.ufjf.dcc193.trabalhoOpcional.trabalhoOpcional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrabalhoOpcionalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
